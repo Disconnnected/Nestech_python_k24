6793057cdbd9dbdcf44ff92858c81356030b43ef
Viktor Nguyen <code.quangnv@gmail.com>
2023-05-22 19:49:28 +0700

    real example

---
+++ pull_request/real_example/max_number_list.py
@@ -0,0 +1,3 @@
+"""
+have a list. return the highest number on a list. not using max()
+"""
---
+++ pull_request/real_example/sorted_list.py
@@ -0,0 +1,4 @@
+"""
+have a list: a = [3,6,8,3,6,99,45,28,61,0,-47]
+sort list with order from lowest to highest
+"""
