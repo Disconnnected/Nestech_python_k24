2d417c0747e8be940b53cd10249ca4f738fd8666
Viktor Nguyen <code.quangnv@gmail.com>
2023-05-22 19:11:44 +0700

    next commit

--- test.py
+++ test.py
@@ -17,5 +17,7 @@ from bs4 import BeautifulSoup
 request = requests.get('https://vnexpress.net/tin-tuc-24h')
 
 html_doc = request.text
+# f = open("web_vnexpress_2.txt", "w", encoding="utf-8")
+# f.write(html_doc)
 soup = BeautifulSoup(html_doc, 'html.parser')
-print(soup.find(id='down-app-popup_vne'))
+print(soup.find(id='sis_slider'))
